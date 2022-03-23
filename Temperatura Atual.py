7368c0996ee5b32a05047e05742d944ccde63b96
Dio30 <101674106+Dio30@users.noreply.github.com>
2022-03-23 14:46:33 -0300

    Update Temperatura Atual.py

--- Temperatura Atual.py
+++ Temperatura Atual.py
@@ -1,9 +1,9 @@
 import requests
 from tkinter import *
 
-API_KEY = "d7c5eb336a9f9bea2f44c4e2cc117f48"
-cidade = "Biguaçu"
-link = f"https://api.openweathermap.org/data/2.5/weather?q={'biguaçu'}&appid={'d7c5eb336a9f9bea2f44c4e2cc117f48'}&lang=pt_br"
+API_KEY = "Coloque sua api key aqui"
+cidade = "rio de janeiro"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={'rio de janeiro'}&appid={'api_key'}&lang=pt_br"
 
 def pegar_temperatura():
     requisicao = requests.get(link)
@@ -12,19 +12,5 @@ def pegar_temperatura():
     temperatura = requisicao_dic['main']['temp'] - 273.15
 
     texto = f'''
-    Biguaçu: {temperatura}'''
+    Rio de Janeiro: {temperatura}'''
     texto_previsoes["text"] = texto
-
-janela = Tk()
-janela.title("Previsão do Tempo em Biguaçu")
-
-texto_orientacao = Label(janela, text="Clique no botão para ver a previsão do tempo")
-texto_orientacao.grid(column=0, row=0, padx=10, pady=10)
-
-botao = Button(janela, text="Buscar previsão do tempo", command=pegar_temperatura)
-botao.grid(column=0, row=1)
-
-texto_previsoes = Label(janela, text="")
-texto_previsoes.grid(column=0, row=2, padx=10, pady=10)
-
-janela.mainloop()
